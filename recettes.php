daef5583a838fa8cee7df0fa0dfb9c51e7f61522
RyanJee17 <ryan.jeewooth@gmail.com>
2023-09-11 09:34:59 +0200

    Les fonctions

--- recettes.php
+++ recettes.php
@@ -45,34 +45,4 @@ $recipes = [
     ],
 ];
 
-
-echo " <h1> Affichage des recettes </h1>";
-
-foreach ($recipes as $recipe) {
-    if ($recipe['is_enabled'] == true) {
-        echo "<h2>" . $recipe['title']. '</h2>';
-        $recipe_email = $recipe['author'];
-        foreach ($users as $user) {
-            if ($recipe_email == $user['email']) {
-                echo '<em>' . $user['full_name'] . '</em> ('. $user['age'] .' ans)';
-            }
-        }
-    }
-}
-
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Affichage des recettes</title>
-</head>
-
-<body>
-    <ul>
-
-    </ul>
-</body>
-
-</html>
