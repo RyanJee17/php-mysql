d03f2c0fa444bf0232c08125e3d9f01751ac7a80
RyanJee17 <ryan.jeewooth@gmail.com>
2023-09-13 11:56:53 +0200

    "Implémentez un système de connexion"

--- contact.php
+++ contact.php
@@ -1,19 +1,20 @@
 <!-- contact.php -->
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Formulaire de Contact</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
 
-        <?php include_once('header.php'); ?>
+    <?php include_once('header.php'); ?>
         <h1>Contactez nous</h1>
         <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
@@ -38,5 +39,4 @@
 
     <?php include_once('footer.php'); ?>
 </body>
-
 </html>
