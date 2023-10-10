36bfa9d67cde3e92bd57da6a3cd39d5ca5acc922
RyanJee17 <ryan.jeewooth@gmail.com>
2023-10-10 09:22:26 +0200

    "Conservez les données grâce aux cookies"

--- contact.php
+++ contact.php
@@ -1,19 +1,20 @@
 <!-- contact.php -->
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Formulaire de Contact</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
 
-        <?php include_once('header.php'); ?>
+    <?php include_once('header.php'); ?>
         <h1>Contactez nous</h1>
         <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
@@ -25,19 +26,17 @@
                 <label for="message" class="form-label">Votre message</label>
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
-            
-                <!-- Ajout champ d'upload ! -->
-                <div class="mb-3">
+            <!-- Ajout champ d'upload ! -->
+            <div class="mb-3">
                 <label for="screenshot" class="form-label">Votre capture d'écran</label>
                 <input type="file" class="form-control" id="screenshot" name="screenshot" />
-                </div>
-                <!-- Fin ajout du champ -->
-                <button type="submit" class="btn btn-primary">Envoyer</button>
-            </form>
+            </div>
+            <!-- Fin ajout du champ -->
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
         <br />
     </div>
 
     <?php include_once('footer.php'); ?>
 </body>
-
 </html>
